939954eddc0ab35dfff713346dff188d8af63f58
Tomasz Belina <tomasz.belina@qualent.eu>
2016-02-07 14:39:33 +0100

    Dodaj przekazywanie parametrow routingu

--- app/components/product.ts
+++ app/components/product.ts
@@ -1,6 +1,18 @@
 import {Component} from 'angular2/core';
+import {RouteParams} from 'angular2/router';
+
 @Component({
 	selector: 'product',
-	template: '<h1 class="product">Product Detail Component</h1>',
+	template: `<h1 class="product">Product Detail for Product: {{productID}}</h1>` ,
 	styles: ['.product {background: cyan}']})
-export class ProductDetailComponent {}
+export class ProductDetailComponent{
+
+	productID: string;
+
+	constructor(params: RouteParams) {
+		this.productID = params.get('id');
+	}
+}
+
+
+
